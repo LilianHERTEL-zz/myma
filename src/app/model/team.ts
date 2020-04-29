678274c76c6663dfd213351884d6fddcfd6150f2
Lilian <lilian.hertel@etu.uca.fr>
2020-04-29 12:01:17 +0200

    Bootstrap & jquery fixed, images fixed

--- src/app/model/team.ts
+++ src/app/model/team.ts
@@ -2,12 +2,12 @@ import { Member } from './member';
 
 export class Team {
   team: Array<Member> = [
-    new Member('Leon', 'CEO', '/assets/images/leon.png'),
-    new Member('Alan', 'CTO', '/assets/images/alan.png'),
-    new Member('Lewis', 'Lead App Developer', '/assets/images/lewis.png'),
-    new Member('Andrew', 'Deep Learning Engineer', '/assets/images/andrew.png'),
-    new Member('Miriam', 'CMO', '/assets/images/miriam.png'),
-    new Member('Craig', 'USA Lead', '/assets/images/craig.png'),
-    new Member('Laurence', 'CFO', '/assets/images/laurence.png'),
+    new Member('Leon', 'CEO', 'assets/images/leon.png'),
+    new Member('Alan', 'CTO', 'assets/images/alan.png'),
+    new Member('Lewis', 'Lead App Developer', 'assets/images/lewis.png'),
+    new Member('Andrew', 'Deep Learning Engineer', 'assets/images/andrew.png'),
+    new Member('Miriam', 'CMO', 'assets/images/miriam.png'),
+    new Member('Craig', 'USA Lead', 'assets/images/craig.png'),
+    new Member('Laurence', 'CFO', 'assets/images/laurence.png'),
   ];
 }
