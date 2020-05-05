8b262cb7f94671b76a61dec1756b36a1977e8982
Lilian <lilian.hertel@etu.uca.fr>
2020-05-05 16:22:23 +0200

    contact page form added (WIP)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -75,7 +76,12 @@ import { WorkWithComponent } from './components/products/consulting/work-with/wo
     ConsumersInfoComponent,
     WorkWithComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -4,14 +4,30 @@ import { Title } from '@angular/platform-browser';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent implements OnInit {
   title: string = 'Myma ~ Contact';
+
+  /*
+   * Contact form user inputs
+   */
+  name: string;
+  email: string;
+  message: string;
 
   constructor(private titleService: Title) {}
 
   ngOnInit(): void {
     this.titleService.setTitle(this.title);
+  }
+
+  /**
+   * Sends the email - WIP
+   * Only alerting for now
+   */
+  processForm() {
+    const allInfo = `My name is ${this.name}. My email is ${this.email}. My message is ${this.message}`;
+    alert(allInfo);
   }
 }
